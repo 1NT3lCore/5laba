312c664292a6b0b9906142e7084ce2ff3e71eeba
1NT3lCore <148251080+1NT3lCore@users.noreply.github.com>
2024-04-23 15:06:12 +0300

    Update laba5,2.cpp

--- laba5,2/laba5,2.cpp
+++ laba5,2/laba5,2.cpp
@@ -1,12 +1,23 @@
-﻿#include "radio_workshop.h"
+#include "radio_workshop.h"
 
 int main() {
     std::vector<Receipt> receipts;
     fillReceipts(receipts);
 
     std::string group;
-    std::cout << "Enter the group of products to check orders for (Group1, Group2, Group3): ";
-    std::cin >> group;
+    bool validGroup = false;
+
+    while (!validGroup) {
+        std::cout << "Enter the group of products to check orders for (Group1, Group2, Group3): ";
+        std::cin >> group;
+
+        if (group == "Group1"  group == "Group2"  group == "Group3") {
+            validGroup = true;
+        }
+        else {
+            std::cout << "Invalid group entered. Please enter Group1, Group2, or Group3." << std::endl;
+        }
+    }
 
     printReceipts(receipts, group);
 
